9322c5982c371c05c8c451be3a57ce16abe8055b
Alber <alberdsp@gmail.com>
2023-09-27 23:14:47 +0200

    inicio

---
+++ src/modelo/Vehiculo.java
@@ -0,0 +1,41 @@
+package modelo;
+
+
+
+/**
+ * @author Alber 2023 clase Vehiculo
+ * 
+ */
+
+
+
+
+public abstract class Vehiculo {
+	
+	protected String marca;
+	protected String matricula;
+	protected double kilometros;
+	protected Integer cap_deposito;
+	protected Integer estado_deposito;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		// aquí el la lógica
+		
+		};
+	
+	
+	
+	
+	
+
+}
